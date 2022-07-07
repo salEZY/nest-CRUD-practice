4ab083d2d6bb56b716450f933a51322bf1fb266b
salEZY <bobicaleks@yahoo.com>
2022-07-07 19:27:57 +0200

    add custom logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
@@ -8,4 +10,14 @@ import { ProductsModule } from './products/products.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Several usages:
+    //// Middleware for all controllers
+    consumer.apply(LoggerMiddleware).forRoutes(ProductsController)
+    //// Specific HTTP request ONLY(GET for example)
+    //consumer.apply(LoggerMiddleware).forRoutes({ path: 'products', method: RequestMethod.GET })
+    //// Exclude middleware for specific HTTP requests
+    //consumer.apply(LoggerMiddleware).exclude({ path: 'products', method: RequestMethod.GET }).forRoutes(ProductsController)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  // register global middleware here
+  // app.use(cors(), helmet(), morgan()) - EXAMPLE
+
   await app.listen(3000);
 }
 bootstrap();
